6aa4bb4e7cb56ce69f997fa251f744e421c0e7fd
Nicholas Charriere <nicholascharriere@gmail.com>
2026-03-12 13:46:34 -0700

    Fix copilot and tab behavior

--- lua/community.lua
+++ lua/community.lua
@@ -7,6 +7,5 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.colorscheme.catppuccin" },
-  { import = "astrocommunity.completion.copilot-lua-cmp" },
   -- import/override with your plugins folder
 }
--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -2,6 +2,46 @@ return {
   {
     "github/copilot.vim",
     event = "InsertEnter",
-    autoStart = true,
+    init = function()
+      vim.g.copilot_no_tab_map = true
+      vim.g.copilot_assume_mapped = true
+    end,
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function(_, opts)
+      local cmp = require "cmp"
+
+      if not opts.mapping then opts.mapping = {} end
+
+      opts.mapping["<Tab>"] = cmp.mapping(function(fallback)
+        -- 1. If Copilot has a visible suggestion, accept it
+        local copilot_keys = vim.fn["copilot#Accept"]("")
+        if copilot_keys ~= "" then
+          vim.api.nvim_feedkeys(copilot_keys, "i", true)
+        -- 2. If a snippet is expandable/jumpable, do that
+        elseif vim.snippet and vim.snippet.active { direction = 1 } then
+          vim.snippet.jump(1)
+        -- 3. If cmp menu is visible, select next item
+        elseif cmp.visible() then
+          cmp.select_next_item()
+        -- 4. Otherwise, insert a normal Tab
+        else
+          fallback()
+        end
+      end, { "i", "s" })
+
+      opts.mapping["<S-Tab>"] = cmp.mapping(function(fallback)
+        if vim.snippet and vim.snippet.active { direction = -1 } then
+          vim.snippet.jump(-1)
+        elseif cmp.visible() then
+          cmp.select_prev_item()
+        else
+          fallback()
+        end
+      end, { "i", "s" })
+
+      return opts
+    end,
   },
 }
