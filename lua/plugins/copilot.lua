0b5c4290bc5601b88b83552e33a6eab135094b47
Nicholas Charriere <nicholascharriere@gmail.com>
2026-03-15 09:40:22 -0700

    Fix lazy cache and change install hook for copilot to lazy from insertEnter

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,7 +1,7 @@
 return {
   {
     "github/copilot.vim",
-    event = "InsertEnter",
+    event = "VeryLazy",
     init = function()
       vim.g.copilot_no_tab_map = true
       vim.g.copilot_assume_mapped = true
